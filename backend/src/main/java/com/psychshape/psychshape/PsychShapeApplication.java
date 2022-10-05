0caf2b756b3fb7037e8cdf29a914effe90b191fb
lorin557799 <77257219+lorin557799@users.noreply.github.com>
2022-10-05 18:50:20 -0500

    MySQL connected, cleaned up code

--- backend/src/main/java/com/psychshape/psychshape/PsychShapeApplication.java
+++ backend/src/main/java/com/psychshape/psychshape/PsychShapeApplication.java
@@ -2,11 +2,8 @@ package com.psychshape.psychshape;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class PsychShapeApplication {
 
 	public static void main(String[] args) {
